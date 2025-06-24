9bd78a5dc6082006822db57f3239166aec60d866
Progna-Labony-Roy <labony.progna@gmail.com>
2025-06-25 01:25:02 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Tailwind + DaisyUI</h1>
+      <button className="btn btn-primary mt-4">Click me</button>
+    </div>
+  )
+}
+
+export default App;
