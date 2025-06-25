d2b75995eab05754d032bc3b2c7a0777edc16b4a
Progna-Labony-Roy <labony.progna@gmail.com>
2025-06-25 10:20:02 +0600

    slider added

---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,51 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+import img1 from '../../../assets/home/img1.jpg';
+import img2 from '../../../assets/home/img2.jpg';
+import img3 from '../../../assets/home/img3.jpg';
+import img4 from '../../../assets/home/img4.jpg';
+import img6 from '../../../assets/home/img6.jpg';
+
+
+const Category = () => {
+    return (
+        <div className='my-16 text-center'>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src={img1} alt="" />
+                    <h3 className='text-3xl uppercase text-center -mt-16 text-white'>Salads</h3>
+                </SwiperSlide>
+                <SwiperSlide><img src={img1} alt="" />
+                <h3 className='text-3xl uppercase text-center -mt-16 text-white'>Salads</h3></SwiperSlide>
+                <SwiperSlide><img src={img2} alt="" />
+                <h3 className='text-3xl uppercase text-center -mt-16 text-white'>Pizza</h3></SwiperSlide>
+                <SwiperSlide><img src={img3} alt="" />
+                <h3 className='text-3xl uppercase text-center -mt-16 text-white'>Soup</h3></SwiperSlide>
+                <SwiperSlide><img src={img4} alt="" />
+                <h3 className='text-3xl uppercase text-center -mt-16 text-white'>Cake</h3></SwiperSlide>
+                <SwiperSlide><img src={img6} alt="" />
+                <h3 className='text-3xl uppercase text-center -mt-16 mb-16 text-white'>Salads</h3></SwiperSlide>
+                {/* <SwiperSlide><img src={img1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img1} alt="" /></SwiperSlide> */}
+            </Swiper>
+        </div>
+    );
+};
+
+export default Category;
